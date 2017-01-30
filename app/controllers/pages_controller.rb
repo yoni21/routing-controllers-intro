ad0f1f1dc4f89b42475fc2438025b898e053c602
yonas samuel <yonastlolo@yahoo.com>
2017-01-30 15:05:46 -0500

    routing_control

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,42 @@
 class PagesController < ApplicationController
-end
+    before_action :set_kitten_url, only: [:kitten, :kittens]
+
+  def welcome
+     @header = "This is the welcome page"
+
+    render :about
+  end
+
+   def about
+     @header = "10 years in business"
+   end
+
+   def kittens
+     @header = "what"
+     set_kitten_url
+   end
+
+   def kitten
+     @header = "like this rail thing"
+     set_kitten_url
+   end
+
+   def set_kitten_url
+     requested_size = params[:size]
+     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+   end
+
+   def contest
+     @header = "we win"
+     flash[:notice] = "Sorry, the contest has ended"
+     redirect_to welcome_url
+   end
+
+   def secrets
+     unless params [:magic_word] == "yonas"
+
+     flash[:notice] = "Sorry, you're not authorized to see that page!"
+     redirect_to welcome_url
+   end
+ end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#welcome'
+
+  get '/welcome' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
+  get '/contest' => 'pages#contest'
+
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+
+  get '/kittens/:size' => 'pages#kittens'
+
+  get '/secrets/:magic_word' => 'pages#secrets'
+
 end
